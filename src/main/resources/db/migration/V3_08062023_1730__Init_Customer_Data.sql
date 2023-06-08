56b8b7c50d2272512adc26cedabe313a35d24707
hendisantika <hendisantika@yahoo.co.id>
2023-06-08 17:38:45 +0700

    #23 - Add Init Customer Data ✏️📗:octocat:🐸

---
+++ src/main/resources/db/migration/V3_08062023_1730__Init_Customer_Data.sql
@@ -0,0 +1,11 @@
+insert into customer (first_name, last_name, email, country, is_active)
+values ('Tara', 'Wheeliker', 'twheeliker0@storify.com', 'Brazil', 'false'),
+       ('Daisy', 'Klulisek', 'dklulisek1@constantcontact.com', 'Peru', 'true'),
+       ('Fredericka', 'Marusik', 'fmarusik2@google.ca', 'China', 'true'),
+       (NULL, 'Pinckstone', 'mpinckstone3@prlog.org', 'Philippines', 'true'),
+       ('Dela', 'Dacks', 'ddacks4@hao123.com', 'China', 'false'),
+       ('Brita', 'Doughton', 'bdoughton5@tmall.com', 'China', 'false'),
+       ('Jamima', 'Sorsby', 'jsorsby6@goo.ne.jp', 'Poland', 'true'),
+       ('Vincenty', 'Ianinotti', 'vianinotti7@freewebs.com', 'Israel', 'false'),
+       ('Karin', 'Sollas', 'ksollas8@cdc.gov', 'Sweden', 'false'),
+       ('Foss', 'Bifield', 'fbifield9@statcounter.com', 'Philippines', 'false');
