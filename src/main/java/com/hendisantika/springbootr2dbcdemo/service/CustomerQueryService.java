3cf64d3d743e93b41b95c9f5f49bcae6fa984e70
hendisantika <hendisantika@yahoo.co.id>
2023-06-08 17:43:30 +0700

    #27 - Add search in Customer Query Service ✏️📗:octocat:🐸

--- src/main/java/com/hendisantika/springbootr2dbcdemo/service/CustomerQueryService.java
+++ src/main/java/com/hendisantika/springbootr2dbcdemo/service/CustomerQueryService.java
@@ -1,8 +1,14 @@
 package com.hendisantika.springbootr2dbcdemo.service;
 
+import com.hendisantika.springbootr2dbcdemo.entity.Customer;
 import com.hendisantika.springbootr2dbcdemo.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +25,22 @@ import org.springframework.stereotype.Service;
 public class CustomerQueryService {
 
     private final CustomerRepository customerRepository;
+
+    private final Map<String, String> countryMap = Map.of(
+            "BR", "Brazil",
+            "CH", "China",
+            "ID", "Indonesia",
+            "MY", "Malaysia",
+            "PH", "Philippines",
+            "JP", "Japan",
+            "KR", "Korea",
+            "SG", "Singapore",
+            "US", "United States"
+    );
+
+    public Flux<Customer> search(Customer customer) {
+        ExampleMatcher matcher = ExampleMatcher.matching()
+                .withTransformer("country", op -> op.map(c -> countryMap.getOrDefault(c, "UNKNOWN")));
+        return this.customerRepository.findAll(Example.of(customer, matcher));
+    }
 }
