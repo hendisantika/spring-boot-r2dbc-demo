d68996f9ccf2df693c3b374d0072f86989057f89
hendisantika <hendisantika@yahoo.co.id>
2023-06-08 17:37:42 +0700

    #22 - Add Init Customer Table ✏️📗:octocat:🐸

---
+++ src/main/resources/db/migration/V2_08062023_1728__Init_Customer_Table.sql
@@ -0,0 +1,9 @@
+create table customer
+(
+    id         INT auto_increment,
+    first_name VARCHAR(50),
+    last_name  VARCHAR(50),
+    email      VARCHAR(50),
+    country    VARCHAR(50),
+    is_active  Boolean
+);
